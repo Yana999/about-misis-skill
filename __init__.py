9fc6ebe79924f002e5879089ca2ee946bb289562
yana999 <abdramanova.yana@yandex.ru>
2022-12-23 04:04:06 +0300

    finding error

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
 import requests
 
-
-
 class AboutMisis(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
@@ -11,26 +9,29 @@ class AboutMisis(MycroftSkill):
     @intent_file_handler('misis.about.intent')
     def handle_misis_about(self, message):
         utt = message.data.get('utterance')
-        # url = "http://misisrobot.live:8000/text-ask-text"
-        # payload = {'question': utt}
+        # url = "http://misisrobot.live:8000/text-ask-text?question=" + utt
         # r = requests.get(url)
-        # self.speak(r.text)
+        # self.speak(r.json()['answer'])
         utt = str(utt)
-        utt = utt.lower()
-        if(utt.find("вопрос о МИСиС") >= 0):
-            utt = utt[14:]
-        if (utt.find("о МИСиС") >= 0):
-            utt = utt[7:]
-        if (utt.find("МИСиС") >= 0):
-            utt = utt[5:]
-        if (utt.find("есть вопрос") >= 0):
-            utt = utt[11:]
-        if (utt.find("хочу задать вопрос") >= 0):
-            utt = utt[18:]
-        if (utt.find("робот") >= 0):
-            utt = utt[5:]
+        # utt = utt.lower()
+        # if(utt.find("вопрос о МИСиС") >= 0):
+        #     utt = utt[14:]
+        # if (utt.find("о МИСиС") >= 0):
+        #     utt = utt[7:]
+        # if (utt.find("МИСиС") >= 0):
+        #     utt = utt[5:]
+        # if (utt.find("есть вопрос") >= 0):
+        #     utt = utt[11:]
+        # if (utt.find("хочу задать вопрос") >= 0):
+        #     utt = utt[18:]
+        # if (utt.find("робот") >= 0):
+        #     utt = utt[5:]
         self.speek(utt)
 
 def create_skill():
     return AboutMisis()
 
+# if __name__ == '__main__':
+#     url = "http://misisrobot.live:8000/text-ask-text?question=" + "направления"
+#     r = requests.get(url)
+#     print(r.json()['answer'])
