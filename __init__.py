de40f99ee451537f2cba85950ece713005322620
yana999 <abdramanova.yana@yandex.ru>
2023-05-07 23:04:17 +0300

    попытка собрать все в один файл, не видит другие

--- __init__.py
+++ __init__.py
@@ -1,14 +1,27 @@
 import logging
+from pathlib import Path
+from typing import Optional
 
+from deeppavlov import Chainer, train_model, build_model
+from deeppavlov.core.common.file import read_json
 from mycroft import MycroftSkill, intent_file_handler
-from tools import VOATools
-from config import VOAConfig
+from pydantic import Field
+
 
 class AboutMisis(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         config = VOAConfig()  # type: ignore
-        voaTools = VOATools(config)
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+        ml_root_path: str = Field(..., env="ML_ROOT_PATH")
+        ml_config_path: str = Field(..., env="ML_CONFIG_FILE")
+
+        self.tmp_dir = Path(Field(..., env="TMP_DIR"))
+        self.tmp_dir.mkdir(exist_ok=True, parents=True)
+
+        self._ml_path = config.ml_config_path
+        self._predictor: Optional[Chainer] = None
 
     @intent_file_handler('misis.about.intent')
     def handle_misis_about(self, message):
@@ -33,6 +46,60 @@ class AboutMisis(MycroftSkill):
         r = self.voaTools.voa_text(utt)
         logging.info("Полученный ответ: " + r.a)
         self.speak(r.json()['answer'])
+
+
+    def _load_model(self, config_path: str) -> Chainer:
+        """Load or train deeppavlov model."""
+        model_config = read_json(config_path)
+        model_config["metadata"]["variables"]["ROOT_PATH"] = (
+            self.config.ml_root_path
+        )
+        try:
+            self._predictor = build_model(model_config, load_trained=True)
+        except FileNotFoundError:
+            self._predictor = train_model(model_config, download=True)
+        return self._predictor
+
+    def load_model(self, config_path: Optional[str] = None) -> Chainer:
+        """Return deeppavlov model."""
+        if config_path is None or config_path == self._ml_path:
+            if self._predictor:
+                return self._predictor
+
+        if not config_path:
+            config_path = self._ml_path
+        predictor = self._load_model(config_path)
+
+        if config_path != self._ml_path:
+            self._ml_path = config_path
+        return predictor
+
+    def voa_text(
+            self,
+            question: str,
+            default_answer: str = "Извините, не совсем поняла ваш вопрос."
+    ) -> str:
+        """Answer the text question.
+
+        Args:
+            question (str, Path): Question.
+            default_answer (str): Answer if question is not recognized.
+
+        Returns:
+            VOAPredictionResult: result.
+        """
+        predictor = self.load_model()
+        resp = predictor([question])
+
+        answer = resp[0][0]
+        score = resp[1][0]
+        status = True
+
+        if not score:
+            answer = default_answer
+            status = False
+
+        return answer
 
 def create_skill():
     return AboutMisis()
