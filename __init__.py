1ae6e878d69d706133f1961ecebf85b6e6001a98
Yana999 <abdramanova.yana@yandex.ru>
2023-06-29 17:35:32 +0300

    added log reg data

--- __init__.py
+++ __init__.py
@@ -1,27 +1,20 @@
 import json
 import logging
-import socket
 from os.path import join, abspath, dirname
 from pathlib import Path
 from typing import Optional
-from deeppavlov import configs
-from deeppavlov.core.common.file import read_json
-from deeppavlov import configs, train_model
-import openai
+
 from deeppavlov import Chainer, train_model, build_model
 from deeppavlov.core.common.file import read_json
-from mycroft import MycroftSkill, intent_file_handler, intent_handler
-from mycroft.skills.intent_service import AdaptIntent
+from mycroft import MycroftSkill, intent_file_handler
 
 
 class AboutMisis(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self, "AboutMISISSkill")
         self.ml_root_path = './data'
-
         self.tmp_dir = Path('./tmp_data')
         self.tmp_dir.mkdir(exist_ok=True, parents=True)
-        openai.api_key = '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'
         self._ml_path = join(abspath(dirname(__file__)), "data", "tfidf_logreg_autofaq_misis.json")
         self._predictor: Optional[Chainer] = None
         self.is_eye = False
@@ -60,7 +53,7 @@ class AboutMisis(MycroftSkill):
             if self.send_eye_check():
                 logging.info("Персона обнаружена")
                 logging.info("вопрос для модели: " + utt)
-                r = self.ask_gpt(utt)
+                r = self.voa_text(utt)
             else:
                 logging.info("Персона потеряна")
                 r = self.to_person
@@ -68,16 +61,6 @@ class AboutMisis(MycroftSkill):
             logging.error("Произошела ошибка " + str(err))
             r = self.error_message
         self.speak(r)
-
-    def ask_gpt(self, question):
-        if question:
-            question = question + ' университет МИСИС'
-            messages = [{"role": "user", "content": question}]
-            chat = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo", messages=messages
-            )
-        reply = chat.choices[0].message.content
-        print(f"ChatGPT: {reply}")
 
     # @intent_handler(AdaptIntent()
     #                 .one_of("привет", "здравствуй", "добрый день", "добрый вечер", "приветствую", "доброе утро", "здравствуйте"))
@@ -92,7 +75,6 @@ class AboutMisis(MycroftSkill):
             logging.error("Произошела ошибка " + str(err))
             self.speak(self.error_message)
 
-
     # @intent_handler(AdaptIntent()
     #                 .one_of("пока", "до свидания"))
     @intent_file_handler('misis.about.bye')
@@ -106,36 +88,17 @@ class AboutMisis(MycroftSkill):
             logging.error("Произошела ошибка " + str(err))
             self.speak(self.error_message)
 
-    def send_eye_check(self):
-        # logging.info("Отправляем сообщенрие-проверку")
-        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # s.connect((self.TCP_IP, self.TCP_PORT))
-        # s.sendall(bytes(self.MESSAGE, encoding="utf-8"))
-        # data = s.recv(self.BUFFER_SIZE).decode('utf-8')
-        # logging.info("Ответ зрения:" + str(data))
-        # s.close()
-        # if(str(data) == '1'):
-        #     return True
-        # if(str(data) == '0'):
-        #     return False
-        # else:
-        #     raise Exception('Неизвестный тип сообщения от зрения')
-        # return data
-        return 'yes'
-
     def _load_model(self, config_path: str) -> Chainer:
         """Load or train deeppavlov model."""
-        model_config = read_json(configs.doc_retrieval.ru_ranker_tfidf_wiki)
-        model_config["dataset_reader"]["data_path"] = join(abspath(dirname(__file__)), "data", "misis_site")
-        model_config["dataset_reader"]["dataset_format"] = "txt"
-        model_config["train"]["batch_size"] = 1
-
+        model_config = read_json(config_path)
+        model_config["dataset_reader"]["data_url"] = join(abspath(dirname(__file__)), "data", "dataset_misis_qa.csv")
+        model_config["metadata"]["variables"]["ROOT_PATH"] = (
+            self.ml_root_path
+        )
         try:
-            doc_retrieval = train_model(model_config)
-            squad = build_model(join(abspath(dirname(__file__)), "data", "squad_ru_bert.json"), download=True)
-            self._predictor = build_model(join(abspath(dirname(__file__)), "data", "ru_odqa_infer_wiki.json"), download=False)
+            self._predictor = build_model(model_config, load_trained=True)
         except FileNotFoundError:
-            logging.warning("Невозможно загрузить модель")
+            self._predictor = train_model(model_config, download=True)
         return self._predictor
 
     def load_model(self, config_path: Optional[str] = None) -> Chainer:
@@ -152,28 +115,31 @@ class AboutMisis(MycroftSkill):
             self._ml_path = config_path
         return predictor
 
-    # def voa_text(
-    #         self,
-    #         question: str,
-    #         default_answer: str = "Извините, не совсем поняла ваш вопрос."
-    # ) -> str:
-    #     """Answer the text question.
-    #
-    #     Args:
-    #         question (str, Path): Question.
-    #         default_answer (str): Answer if question is not recognized.
-    #
-    #     Returns:
-    #         VOAPredictionResult: result.
-    #     """
-    #     predictor = self.load_model()
-    #     answer = predictor([question])
-    #
-    #     logging.info("Полученный ответ: " + answer)
-    #     if not answer or answer == '':
-    #         answer = default_answer
-    #
-    #     return answer
+    def voa_text(
+            self,
+            question: str,
+            default_answer: str = "Извините, не совсем поняла ваш вопрос."
+    ) -> str:
+        """Answer the text question.
+
+        Args:
+            question (str, Path): Question.
+            default_answer (str): Answer if question is not recognized.
+
+        Returns:
+            VOAPredictionResult: result.
+        """
+        predictor = self.load_model()
+        resp = predictor([question])
+
+        answer = resp[0][0]
+        score = resp[1][0]
+        logging.info("Полученный ответ: " + answer)
+        logging.info("Score: ")
+        logging.info(score)
+        if not score:
+            answer = default_answer
+        return answer
 
 
 def create_skill():
