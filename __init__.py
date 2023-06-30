fe8f36565bacb4ab323f1f3062892f33e0ac4ee9
Yana999 <abdramanova.yana@yandex.ru>
2023-07-01 01:53:39 +0300

    изменила модель на модель Льва

--- __init__.py
+++ __init__.py
@@ -5,10 +5,19 @@ from pathlib import Path
 from typing import Optional
 import socket
 import time
-
+import pandas as pd
+from torch import nn
+import torch.nn.functional as F
+import torch
+import difflib
+from transformers import BertTokenizer, BertModel
 from deeppavlov import Chainer, train_model, build_model
 from deeppavlov.core.common.file import read_json
 from mycroft import MycroftSkill, intent_handler
+from nltk.corpus import stopwords
+import re
+import random
+from pymorphy2 import MorphAnalyzer
 
 
 class AboutMisis(MycroftSkill):
@@ -17,7 +26,8 @@ class AboutMisis(MycroftSkill):
         self.ml_root_path = './data'
         self.tmp_dir = Path('./tmp_data')
         self.tmp_dir.mkdir(exist_ok=True, parents=True)
-        self._ml_path = join(abspath(dirname(__file__)), "data", "tfidf_logreg_autofaq_misis.json")
+        # self._ml_path = join(abspath(dirname(__file__)), "data", "tfidf_logreg_autofaq_misis.json")
+        self._ml_path = join(abspath(dirname(__file__)), 'lev_model', 'best_model_state.bin')
         self._predictor: Optional[Chainer] = None
         self.is_eye = False
         self.TCP_IP = '192.168.1.101'
@@ -26,6 +36,14 @@ class AboutMisis(MycroftSkill):
         self.MESSAGE = json.dumps({'type': 'eye'})
         self.to_person = 'Пожалуйста, вернитесь в фокус зрения робота'
         self.error_message = 'Есть технический сбой в моих системах произвожу перезагрузку'
+        self.pre_trained_model_ckpt = 'DeepPavlov/rubert-base-cased'
+        self.device = torch.device('cpu')
+        self.patterns = "[A-Za-z0-9!#$%&'()*+,./:;<=>?@[\]^_`{|}~—\"\-]+"
+        self.stopwords_ru = stopwords.words("russian")
+        self.morph = MorphAnalyzer()
+        self.tokenizer = None
+        self.dataset = join(abspath(dirname(__file__)), 'data', '12042023_dataset_sort_change_tokenizer_fix_sort.xlsx')
+        self.class_names = ['поступление - перевод', 'общежитие', 'учебная деятельность', 'внеучебная деятельность', 'документы', 'работа', 'финансы']
 
     @intent_handler('misis.about.intent.intent')
     def handle_misis_about(self, message):
@@ -53,7 +71,7 @@ class AboutMisis(MycroftSkill):
             if self.send_eye_check():
                 logging.info("Персона обнаружена")
                 logging.info("вопрос для модели: " + utt)
-                r = self.voa_text(utt)
+                r = self.make_ans(utt, self.dataset)
             else:
                 logging.info("Персона потеряна")
                 r = self.to_person
@@ -104,7 +122,8 @@ class AboutMisis(MycroftSkill):
     def _load_model(self, config_path: str) -> Chainer:
         """Load or train deeppavlov model."""
         model_config = read_json(config_path)
-        model_config["dataset_reader"]["data_url"] = join(abspath(dirname(__file__)), "data", "07042023_dataset_sort_povtor.csv")
+        model_config["dataset_reader"]["data_url"] = join(abspath(dirname(__file__)), "data",
+                                                          "07042023_dataset_sort_povtor.csv")
         model_config["metadata"]["variables"]["ROOT_PATH"] = (
             self.ml_root_path
         )
@@ -114,7 +133,7 @@ class AboutMisis(MycroftSkill):
             self._predictor = train_model(model_config, download=True)
         return self._predictor
 
-    def load_model(self, config_path: Optional[str] = None) -> Chainer:
+    def load_model(self, config_path: Optional[str] = None):
         """Return deeppavlov model."""
         if config_path is None or config_path == self._ml_path:
             if self._predictor:
@@ -122,11 +141,79 @@ class AboutMisis(MycroftSkill):
 
         if not config_path:
             config_path = self._ml_path
-        predictor = self._load_model(config_path)
+        predictor = self._load_lev_model(config_path)
 
         if config_path != self._ml_path:
             self._ml_path = config_path
         return predictor
+
+    def load_tokenizer(self):
+        if not self.tokenizer:
+            self.tokenizer = BertTokenizer.from_pretrained(self.pre_trained_model_ckpt)
+        return self.tokenizer
+
+    def _load_lev_model(self, config_path: str):
+        class_names = ['поступление - перевод', 'общежитие', 'учебная деятельность', 'внеучебная деятельность',
+                       'документы', 'работа', 'финансы']
+        myModel = SentimentClassifier(len(class_names))
+        myModel.load_state_dict(torch.load(config_path, map_location=self.device))
+        myModel = myModel.to(self.device)
+        return myModel
+
+    def lemmatize(self, doc):
+        doc = re.sub(self.patterns, ' ', doc)
+        tokens = []
+        for token in doc.split():
+            if token and token not in self.stopwords_ru:
+                token = token.strip()
+                token = self.morph.normal_forms(token)[0]
+
+                tokens.append(token)
+        return self.load_tokenizer().encode(" ".join(tokens), add_special_tokens=False)
+
+    def make_ans(self,  question, dataset):
+        encoded_review = self.load_tokenizer().encode_plus(question, max_length=512, add_special_tokens=True,
+                                               return_token_type_ids=False, pad_to_max_length=True,
+                                               return_attention_mask=True,
+                                               truncation=True, return_tensors='pt')
+        input_ids = encoded_review['input_ids'].to(self.device)
+        attention_mask = encoded_review['attention_mask'].to(self.device)
+        predictor = self.load_model()
+        output = predictor(input_ids, attention_mask)
+        _, prediction = torch.max(output, dim=1)
+
+        tToken = self.lemmatize(question)
+        tq = []
+        index = 0
+        probability = 0.00
+        textQ = ''
+        textA = ''
+        questions = []
+        # import main df
+        df = pd.read_excel(dataset)
+
+        for i in range(0, len(df)):
+            if (probability <= self.mySort(tToken, eval(str(df['tq_fix'][i]))) and df['label'][i] == int(prediction)):
+                tq = eval(str(df['tq_fix'][i]))
+                index = i
+                textQ = df['q'][i]
+                textA = df['a'][i]
+                probability = self.mySort(tToken, eval(str(df['tq_fix'][i])))
+                questions.append([tq, index, textQ, textA, probability])
+
+        max_last_elem = max([que[-1] for que in questions])
+        max_lists = [lst for lst in questions if lst[-1] == max_last_elem]
+        rand = random.choice(max_lists)
+        logging.info(f'{question} - Текст основного вопроса' + '\n' +
+                         f'{self.class_names[prediction]} - ИИ класс' + '\n' +
+                         f'{tToken} - Токенизированный текст основного вопроса' + '\n' +
+                         f'{rand[4] * 100}% - Процент сходства с найденным вопросом' + '\n' +
+                         f'{rand[1]} - Номер найденного вопроса' + '\n' +
+                         f'{rand[2]} - Текст найденного вопроса' + '\n' +
+                         f'{rand[0]} - Токенизированный найденный вопрос' + '\n' +
+                         f'{rand[3]} - Ответ к найденному вопросу')
+
+        return rand[3]
 
     def voa_text(
             self,
@@ -154,12 +241,33 @@ class AboutMisis(MycroftSkill):
             answer = default_answer
         return answer
 
+    # make match
+    def mySort(self, s1, s2):
+        matcher = difflib.SequenceMatcher(None, s1, s2)
+        return matcher.ratio()
+
+class SentimentClassifier(nn.Module):
+    def __init__(self, n_classes):
+        super(SentimentClassifier, self).__init__()
+        self.pre_trained_model_ckpt = 'DeepPavlov/rubert-base-cased'
+        self.bert = BertModel.from_pretrained(self.pre_trained_model_ckpt, return_dict=False)
+        self.drop = nn.Dropout(p=0.3)
+        self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
+
+    def forward(self, input_ids, attention_mask):
+        _, pooled_output = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        output = self.drop(pooled_output)
+        return self.out(output)
+
 
 def create_skill():
     return AboutMisis()
 
 # if __name__ == '__main__':
 #     a = AboutMisis()
-#     utt = 'направления'
-#     r = a.voa_text(utt)
+#     utt = 'как подать ддокументы'
+#     r = a.make_ans(utt, a.dataset)
 #     print(r)
